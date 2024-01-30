2ba7aa8d968a02063511d33380c542fb62596bbf
konas122 <2407626470@qq.com>
2024-01-30 16:53:22 +0800

    add generation & weakref

--- src/core_simple.py
+++ src/core_simple.py
@@ -1,3 +1,4 @@
+import weakref
 import numpy as np
 
 
@@ -23,15 +24,32 @@ class Variable:
         self.data = data
         self.grad = None
         self.creator = None
+        self.generation = 0
 
     def set_creator(self, func):
         self.creator = func
+        self.generation = func.generation + 1
     
     def backward(self):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
         
-        funcs = [self.creator]
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                seen_set.add(f)
+                funcs.append(f)
+                funcs.sort(key=lambda x: x.generation)
+                # for i, x in enumerate(funcs):
+                #     if f.generation < x.generation:
+                #         funcs.insert(i, f)
+                #         break
+
+        
+        add_func(self.creator)
+
         while funcs:
             f = funcs.pop()
             gys = [output.grad for output in f.outputs]
@@ -40,10 +58,16 @@ class Variable:
                 gxs = (gxs,)
             
             for x, gx in zip(f.inputs, gxs):
-                x.grad = gx
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
 
                 if x.creator is not None:
-                    funcs.append(x.creator)
+                    add_func(x.creator)
+
+    def clear_grad(self):
+        self.grad = None
 
 
 class Function:
@@ -55,11 +79,12 @@ class Function:
         if not isinstance(ys, tuple):
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
+        self.generation = max([x.generation for x in inputs])
         for output in outputs:
             output.set_creator(self)
 
         self.inputs = inputs
-        self.outputs = outputs
+        self.outputs = [weakref.ref(output) for output in outputs]
 
         return outputs if len(outputs) > 1 else outputs[0]
 
