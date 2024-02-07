b653184f945922f01cd1e8472896ef0cbcb5dcea
konas122 <2407626470@qq.com>
2024-02-07 21:37:51 +0800

    add tanh

---
+++ test/tanh_test.py
@@ -0,0 +1,26 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+
+from dazero import utils
+from dazero import Variable
+import dazero.functions as F
+
+
+x = Variable(np.array(1.0))
+y = F.tanh(x)
+
+x.name = 'x'
+y.name = 'y'
+y.backward(create_graph=True)
+
+iters = 0
+for i in range(iters):
+    gx = x.grad
+    x.zero_grad()
+    gx.backward(create_graph=True)
+
+gx = x.grad
+gx.name = 'gx' + str(iters+1)
+utils.plot_dot_graph(gx, verbose=False, to_file='tanh.png')
