7b647796a9953a1614285c6a2650c2421ee15788
konas122 <2407626470@qq.com>
2024-02-09 23:00:22 +0800

    add Example

---
+++ examples/LinearRegression.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from dazero import Variable
+import dazero.functions as F
+
+lr = 0.1
+iters = 100
+
+def predict(x, W, b):
+    y = F.matmul(x, W) + b
+    return y
+
+
+def mean_squared_loss(x0, x1):
+    diff = x0 - x1
+    return F.sum(diff ** 2) / len(diff)
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    x = np.random.rand(100, 1)
+    y = 5 + 2 * x + np.random.rand(100, 1)
+
+    # plt.scatter(x, y)
+    # plt.show()
+
+    x, y = Variable(x), Variable(y)
+
+    W = Variable(np.random.randn(1, 1))
+    b = Variable(np.random.randn(1))
+
+    for i in range(iters):
+        y_hat = predict(x, W, b)
+        loss = mean_squared_loss(y_hat, y)
+
+        W.zero_grad()
+        b.zero_grad()
+        loss.backward()
+
+        W.data -= lr * W.grad.data
+        b.data -= lr * b.grad.data
+        print(W.data, b.data, loss.data)
+
+    x_pred = np.linspace(0, 1, 100)
+    y_pred = b.data + W.data * x_pred
+    y_pred = y_pred.reshape((100,))
+
+    plt.scatter(x.data, y.data)
+    plt.plot(x_pred, y_pred)
+    plt.show()
