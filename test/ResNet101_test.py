25b0442319bbb0d40a26e2a5832e9e6581a33ec4
konas122 <2407626470@qq.com>
2024-02-15 16:36:15 +0800

    add `pretrained models`

---
+++ test/ResNet101_test.py
@@ -0,0 +1,22 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from PIL import Image
+import dazero
+from dazero.models import ResNet101
+
+
+url = 'https://github.com/oreilly-japan/deep-learning-from-scratch-3/raw/images/zebra.jpg'
+img_path = dazero.utils.get_file(url)
+img = Image.open(img_path)
+
+x = ResNet101.img_preprocess(img)
+x = x[np.newaxis]
+
+model = ResNet101(pretrained=True)
+with dazero.test_mode():
+    y = model(x)
+predict_id = np.argmax(y.data)
+print(predict_id)
+model.plot(x, to_file='resnet101.pdf')
---
+++ test/ResNet50_test.py
@@ -0,0 +1,22 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from PIL import Image
+import dazero
+from dazero.models import ResNet50
+
+
+url = 'https://github.com/oreilly-japan/deep-learning-from-scratch-3/raw/images/zebra.jpg'
+img_path = dazero.utils.get_file(url)
+img = Image.open(img_path)
+
+x = ResNet50.img_preprocess(img)
+x = x[np.newaxis]
+
+model = ResNet50(pretrained=True)
+with dazero.test_mode():
+    y = model(x)
+predict_id = np.argmax(y.data)
+print(predict_id)
+model.plot(x, to_file='resnet50.pdf')
