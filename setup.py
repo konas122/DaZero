587396625644800135a144fcee0446531c69408b
konas122 <2407626470@qq.com>
2024-02-19 11:23:22 +0800

    add `setup.py`

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from dazero import __version__
+
+setup(name='dazero',
+    version=__version__,
+    license='MIT License',
+    install_requires=['numpy', 'matplotlib'],
+    description='Deep Learning Framework from Zero',
+    author='konas122',
+    author_email='2407626470@qq.com',
+    url='',
+    packages=['dazero'],
+)
