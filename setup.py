13ce1fff2101bd58ac4ebefc5535539c979de520
konas122 <2407626470@qq.com>
2024-02-26 21:27:13 +0800

    update `setup.py` & workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from dazero import __version__
 setup(name='dazero',
     version=__version__,
     license='MIT License',
-    install_requires=['numpy', 'matplotlib'],
+    install_requires=['numpy', 'matplotlib', 'urllib3'],
     description='Deep Learning Framework from Zero',
     author='konas122',
     author_email='2407626470@qq.com',
