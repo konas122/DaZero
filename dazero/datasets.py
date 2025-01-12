10ab9bde7b591adfc5dcb910bf97e307fbdf4a91
konas122 <2407626470@qq.com>
2025-01-12 20:37:45 +0800

    update `MNIST` url

--- dazero/datasets.py
+++ dazero/datasets.py
@@ -84,7 +84,7 @@ class MNIST(Dataset):
         super().__init__(train, transform, target_transform)
 
     def prepare(self):
-        url = 'https://github.com/hamlinzheng/mnist/tree/master/dataset/'
+        url = 'https://github.com/konas122/DaZero/releases/tag/v1.1/'
         train_files = {'target': 'train-images-idx3-ubyte.gz',
                        'label': 'train-labels-idx1-ubyte.gz'}
         test_files = {'target': 't10k-images-idx3-ubyte.gz',
