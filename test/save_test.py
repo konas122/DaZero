473504bf1894f121cba6cd07ae43d26b4e8883aa
konas122 <2407626470@qq.com>
2025-03-14 15:42:00 +0800

    delete `MNIST download` in `save_test`

--- test/save_test.py
+++ test/save_test.py
@@ -3,11 +3,11 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from urllib.error import HTTPError
 
-import dazero
+# import dazero
 import dazero.layers as L
 import dazero.functions as F
-from dazero import optimizers
-from dazero import DataLoader, Model
+# from dazero import DataLoader
+from dazero import optimizers, Model
 
 
 class MLP(Model):
@@ -34,13 +34,13 @@ class MLP(Model):
 max_epoch = 1
 batch_size = 100
 
-try:
-    train_set = dazero.datasets.MNIST(train=True)
-    train_loader = DataLoader(train_set, batch_size)
-except HTTPError as e:
-    print("Failed to download the datasets.", file=sys.stderr)
-    print(e, file=sys.stderr)
-    sys.exit(0)
+# try:
+#     train_set = dazero.datasets.MNIST(train=True)
+#     train_loader = DataLoader(train_set, batch_size)
+# except HTTPError as e:
+#     print("Failed to download the datasets.", file=sys.stderr)
+#     print(e, file=sys.stderr)
+#     sys.exit(0)
 
 model = MLP(784, (1000, 10))
 optimizer = optimizers.SGD(model)
@@ -48,18 +48,18 @@ optimizer = optimizers.SGD(model)
 if os.path.exists('my_mlp.npz'):
     model.load_weights('my_mlp.npz')
 
-for epoch in range(max_epoch):
-    sum_loss = 0
+# for epoch in range(max_epoch):
+#     sum_loss = 0
 
-    for x, t in train_loader:
-        y = model(x)
-        loss = F.softmax_cross_entropy(y, t)
-        model.zero_grad()
-        loss.backward()
-        optimizer.step()
-        sum_loss += float(loss.data) * len(t)
+#     for x, t in train_loader:
+#         y = model(x)
+#         loss = F.softmax_cross_entropy(y, t)
+#         model.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         sum_loss += float(loss.data) * len(t)
 
-    print('epoch: {}, loss: {:.4f}'.format(
-        epoch + 1, sum_loss / len(train_set)))
+#     print('epoch: {}, loss: {:.4f}'.format(
+#         epoch + 1, sum_loss / len(train_set)))
 
 model.save_weights('my_mlp.npz')
